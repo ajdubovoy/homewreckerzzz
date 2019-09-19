49978a0e992c3ca18cd1ef63c24ba4a2fce499c9
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-09-19 13:12:05 +0200

    continue on sass setup

--- app.js
+++ app.js
@@ -4,14 +4,34 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
-const autoprefixer = require('autoprefixer');
-const postcss = require('postcss');
+var postcssMiddleware = require('postcss-middleware');
+var autoprefixer = require('autoprefixer');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
-// view engine setup
+// SASS
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public'),
+  prefix: '/stylesheets',
+  indentedSyntax: true, // true = .sass and false = .scss
+  sourceMap: true
+}));
+app.use(postcssMiddleware({
+  plugins: [
+    autoprefixer({
+      browsers: 'last 2 versions', 
+      cascade: false 
+    })
+  ],
+  src: function(req) {
+    return path.join(path.join(__dirname, 'public', 'stylesheets'), req.url);
+  }
+}));
+
+// PUG Views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -19,21 +39,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(sassMiddleware({
-  src: path.join(__dirname, './sass'),
-  dest: path.join(__dirname, '../public/stylesheets'),
-  prefix: '/stylesheets',
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
-}));
 app.use(express.static(path.join(__dirname, 'public')));
-postcss([ autoprefixer ]).process(css).then(result => {
-  result.warnings().forEach(warn => {
-    console.warn(warn.toString())
-  })
-  console.log(result.css)
-});
 
+// Routers
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
