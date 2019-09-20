9c9d46527de75f079e941fafc1d01ddf59ee2b51
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-09-20 17:50:56 +0200

    migrate old code

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Bootstrap from 'bootstrap-vue';
 import App from './App.vue';
-import Index from './pages/Index.vue';
-import Llama from './pages/Llama.vue';
+import Index from './pages/Index';
+import Llama from './pages/Llama';
+import Play from './pages/Play';
 
 Vue.config.productionTip = false;
 
 const routes = [
   { path: '/', component: Index },
-  { path: '/lotta-llama', component: Llama }
+  { path: '/lotta-llama', component: Llama },
+  { path: '/play', component: Play }
 ];
 
 const router = new Router({
