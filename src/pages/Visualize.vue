1d3865a23d7e41bb31e758190df56ac081515b7a
greg <greg@srlabs.de>
2019-09-28 20:52:46 +0200

    visualiztion for quizzes

--- src/pages/Visualize.vue
+++ src/pages/Visualize.vue
@@ -5,13 +5,22 @@
 <script>
 import p5 from 'p5';
 import {colors} from '../data/instruments.js';
-import {ShimmerSquare} from '../processing/Particle.js';
+import {ShimmerSquare, PulseSquare} from '../processing/Particle.js';
+import remove from "lodash.remove";
+import quiz from "../data/quizzes.js";
 
 export default {
   data() {
     return {
       sketch: null,
-      queue: [1,2,3]
+      queue: [
+        {
+          user: "hi",
+          color: 200,
+          sustain: true
+        }
+      ],
+      users: ['hi']
     }
   },
   mounted() {
@@ -21,6 +30,7 @@ export default {
       let p5 = _p5;
       var particles = [];
       var f = 0;
+      var sustain = [];
 
       p5.setup = function() {
         var cnv = p5.createCanvas(510, 510); 
@@ -34,45 +44,84 @@ export default {
         particles.forEach((p, index, arr) => {
           p.display();
           p.update();
-          if (p.isDead()) {
+        });
+        sustain.forEach((p, index, arr) => {
+          p.p.display();
+          p.p.update();
+          if(!self.users.includes(p.user)) {
             arr.splice(index, 1);
           }
         });
-        var random = Math.random(1);
-        /*if(random < 0.8 && f % 30 == 0) {
-          for(var i = 0; i < 30; i++) {
-            var randX = Math.random()*window.innerWidth;
-            var randY = Math.random()*window.innerHeight;
-            particles.push(new ShimmerSquare(
-              p5.color(240, 200,255), 
-              randX, 
-              randY, 
-              parseInt(60 + random*120), 
-              p5
-            ));
-          }
-        }*/
-        if(self.queue) {
-          console.log(self.queue);
+        if(self.queue.length != 0) {
+          self.queue.forEach((el) => {
+            if(self.users.includes(el.user) && el.sustain) {
+              sustainCloud(el.color, el.user);
+            } else {
+              cloud(el.color);
+            }
+          })
           self.queue = [];
-          particles.push(new ShimmerSquare(
-            p5.color(240, 200,255), 
-            50, 
-            50, 
-            parseInt(60 + random*120), 
-            p5
-          ));
         }
         f++;
       }
       p5.windowResized = function() {
         p5.resizeCanvas(window.innerWidth, window.innerHeight);
       }
+
+      function cloud(hue) {
+        var centerX = Math.random()*window.innerWidth;
+        var centerY = Math.random()*window.innerHeight;
+        for(var i = 0; i < 10; i++) {
+          let randX = centerX + (Math.random() * 200) - 100;
+          let randY = centerY + (Math.random() * 200) - 100;
+          particles.push(new ShimmerSquare(
+            p5.color(hue, 200,255), 
+            randX, 
+            randY, 
+            parseInt(30 + Math.random()*30), 
+            p5
+          ));
+        }
+      }
+      function sustainCloud(hue, user) {
+        var centerX = Math.random()*window.innerWidth;
+        var centerY = Math.random()*window.innerHeight;
+        for(var i = 0; i < 10; i++) {
+          let randX = centerX + (Math.random() * 200) - 100;
+          let randY = centerY + (Math.random() * 200) - 100;
+          sustain.push({user: user, p: new PulseSquare(
+            p5.color(hue, 200,255), 
+            randX, 
+            randY, 
+            parseInt(30 + Math.random()*30), 
+            p5
+          )});
+        }
+      }
     }
   },
   sockets: {
     clientWasPlayed(payload) {
-      console.log(payload.options.controls.waveType);
+      console.log(payload);
+      let obj = {
+        user: payload.token,
+        color: colors[payload.options.controls.waveType].hue,
+        sustain: payload.options.controls.sustain
+      }
+      this.users.push(payload.token);
+      this.queue.push(obj);
+    },
+    clientWasKilled(payload) {
+      var arr = remove(this.users, (el) => el === payload.token);
+      this.users = arr;
+    },
+    quizTally(payload) {
+      var self = this;
+      let quiz = payload.quiz;
+      let responses = payload.responses;
+      responses.forEach((el) => {
+        self.queue.push({user: "", color: payload.quiz.colors[el], sustain: false});
+      })
     }
   }
 }
