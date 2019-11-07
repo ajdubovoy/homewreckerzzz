b51f08c02b9e0d25d1e15f1143cbdae74581f93c
greg <greg@srlabs.de>
2019-11-08 00:16:35 +0100

    small changes

--- srv/index.js
+++ srv/index.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 export default (app, http) => {
   app.use(express.json());
   app.use(cors());
-  app.use('/uploads', express.static(__dirname + '/public/uploads'));
+  app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
   let fileCount = 0;
 
