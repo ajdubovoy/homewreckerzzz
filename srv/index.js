c573cd1775e2ca9099b8d820fd2fbe9fffa897d2
greg <greg@srlabs.de>
2019-11-08 00:51:38 +0100

    blah

--- srv/index.js
+++ srv/index.js
@@ -34,8 +34,8 @@ export default (app, http) => {
   //explicitly serve file using endpoint
   app.get('/uploads/:file', (req,res) => {
     console.log(path.join("public/uploads/", req.params.file));
-    res.contentType(path.join("public/uploads/", req.params.file));
-    console.log('we set the content type');
+    //res.contentType(path.join("public/uploads/", req.params.file));
+    //console.log('we set the content type');
     req.sendFile(path.join("public/uploads/", req.params.file));
   })
 
